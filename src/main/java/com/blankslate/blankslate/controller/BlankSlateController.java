195d2c99b544f8754efd504341ca2d297ab9ab29
Ashish Mishra <ashishmishra037@gmail.com>
2020-10-30 17:37:28 +0530

    Modifying the controller to return response on default call

---
+++ src/main/java/com/blankslate/blankslate/controller/BlankSlateController.java
@@ -0,0 +1,22 @@
+package com.blankslate.blankslate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/")
+public class BlankSlateController {
+
+  @GetMapping("/")
+  public String index() {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MMM/dd HH:mm:ss");
+    Date date = new Date();
+    String response = "Hi, From Blank Slate - SpringBoot App, Current Time is:: "+dateFormat.format(date);
+    return response;
+  }
+}
